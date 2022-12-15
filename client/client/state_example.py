6ff36c953b1342910969354812e438b79b10c6d3
арсений гавриков <arseny20022002@gmail.com>
2022-12-15 10:45:40 +0300

    ddd

--- client/client/state_example.py
+++ client/client/state_example.py
@@ -1,19 +1,25 @@
 class statement:
-    tree = [[1, 4], [2, 3], [5], [5], [5], [0]]
-    reverse_tree = [[], [0], [1], [1], [0], [2, 3, 4]]
-    def __init__(self, current, last):
+    tree = ([1, 4], [2, 3], [5], [5], [5], [0])
+    reverse_tree = ([], [0], [1], [1], [0], [2, 3, 4])
+    def __init__(self, current):
         self.current = current
-        self.last = last
-    def next(self, tree):
-        return tree[self.current]
+    def next_var(self):
+        return statement.tree[self.current]
+    def prev_var(self):
+        return statement.reverse_tree[self.current]
     def set(self, value):
-        self.last = self.current
         self.current = value 
+   
+    
 
 
-A = statement(0, None)
+A = statement(0)
 
-print (A.next(statement.tree))
+print (A.next_var())
+txt = input("введите следующий элемент")
+A.set(int(txt))
+print(A.current)
+print(A.next_var())
 
 
 
