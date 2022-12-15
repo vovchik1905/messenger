41d09ce937202c8bf8fdd3a48836f330ab8c5a6e
арсений гавриков <arseny20022002@gmail.com>
2022-12-15 15:20:31 +0300

    ggg

--- server/settings/config/db_config.py
+++ server/settings/config/db_config.py
@@ -1,6 +1,6 @@
 class DbConfig:
     DATABAZE = "SimpleMessager_v1"
     USER = "postgres"
-    PASSWORD = "Voviknvovikn1905"
+    PASSWORD = "Ars19293949"
     HOST = "127.0.0.1"
     PORT = "5432"
