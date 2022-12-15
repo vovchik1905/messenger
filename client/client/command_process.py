b671c691a3eea762bb94fb3868c246effa688171
арсений гавриков <arseny20022002@gmail.com>
2022-12-15 13:54:54 +0300

    aaaa

--- client/client/command_process.py
+++ client/client/command_process.py
@@ -1,41 +1,45 @@
 from state_class import state
+from user_class import user
+from psycopg2 import OperationalError
 
-def func0(x):
-    print('0' + str(x))
-def func1(x):
-    print('1' + str(x))
-def func2(x):
-    print('2' + str(x))
-def func3(x):
-    print('3' + str(x))
-def func4(x):
-    print('4' + str(x))
-def func5(x):
-    print('5' + str(x))
-def func6(x):
-    print('6' + str(x))
-def func7(x):
-    print('7' + str(x))
-def func8(x):
-    print('8' + str(x))
-def func9(x):
-    print('9' + str(x))
-def func10(x):
-    print('10' + str(x))
-def func11(x):
-    print('11' + str(x))
-def func12(x):
-    print('12' + str(x))
-def func13(x):
-    print('13' + str(x))
-def func14(x):
-    print('14' + str(x))
-def func15(x):
-    print('15' + str(x))
+class func:
+    def func0(User:user):
+            User.state = 0
+    def func1(User:user):
+            User.state = 1
+    def func2(User:user):
+            User.state = 2
+    def func3(User:user):
+            User.state = 3
+    def func4(User:user):
+            User.state = 4
+    def func5(User:user):
+            User.state = 5
+    def func6(User:user):
+            User.state = 6
+    def func7(User:user):
+            User.state = 7
+    def func8(User:user):
+            User.state = 8
+    def func9(User:user):
+            User.state = 9
+    def func10(User:user):
+            User.state = 10
+    def func11(User:user):
+            User.state = 11
+    def func12(User:user):
+            User.state = 12
+    def func13(User:user):
+            User.state = 13
+    def func14(User:user):
+            User.state = 14
+    def func15(User:user):
+            User.state = 15
 
-state_func = {'START':func0, 'SING_IN':func1, 'SING_UP':func2, 'LOGIN_IN':func3, 'LOGIN_UP':func4
-            , 'PASS_UP':func5, 'PASS_REPEAT':func6, 'PASS_IN':func7, 'WAIT_FOR_SELECT':func8, 'SELECT_CHAT':func9
-            , 'CREATE_CHAT':func10 ,'SELECT_USER':func11, 'WAIT_IN_CHAT':func12, 'SEND_HISTORY':func13
-            , 'SEND_MESSAGE':func14, 'GET_MESSAGE':func15}
-
-state_func.get(state.state_names[5])(10)
+state_func = {'START':func.func0, 'SING_IN':func.func1, 'SING_UP':func.func2, 'LOGIN_IN':func.func3, 'LOGIN_UP':func.func4
+            , 'PASS_UP':func.func5, 'PASS_REPEAT':func.func6, 'PASS_IN':func.func7, 'WAIT_FOR_SELECT':func.func8, 'SELECT_CHAT':func.func9
+            , 'CREATE_CHAT':func.func10 ,'SELECT_USER':func.func11, 'WAIT_IN_CHAT':func.func12, 'SEND_HISTORY':func.func13
+            , 'SEND_MESSAGE':func.func14, 'GET_MESSAGE':func.func15}
+User1 = user('Alex', 0, 10)
+state_func.get(state.state_names[User1.state])(User1)
+print(User1.state)
---
+++ client/client/user_class.py
@@ -0,0 +1,6 @@
+class user:
+    def __init__(self, name, state, id):
+        self.name = name
+        self.state = state
+        self.id = id
+        
